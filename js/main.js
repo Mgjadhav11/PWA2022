9cee8971c8a395df706bf55c84420a61c98114b9
Mgjadhav11 <91558022+Mgjadhav11@users.noreply.github.com>
2022-03-18 12:44:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 function regSW(){
   if('serviceWorker' in navigator){
     navigator.serviceWorker
-    .register('./sw.js')
-    .then(function(){
-      console.log('Service worker: Registered');
-    });
   }
 }
